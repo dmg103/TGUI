ed2ac830c399b8b579dc67493823551dbf966d63
David Martinez <dmg103@alu.ua.es>
2022-04-04 12:02:13 +0200

    [0.10]: Cross compiling for linux and win

--- crossBuild.sh
+++ crossBuild.sh
@@ -19,8 +19,17 @@ rm -r build/
 #Build windows
 mkdir build
 cd build
-cmake CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .. -DTGUI_BACKEND=GLFW_OPENGL3 -DTGUI_SHARED_LIBS=FALSE
+x86_64-w64-mingw32-cmake .. -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DTGUI_BACKEND=GLFW_OPENGL3 -DTGUI_SHARED_LIBS=FALSE -DCMAKE_MAKE_PROGRAM=x86_64-w64-mingw32-make
 make -j4
 cd ..
 mv ./build/lib/libtgui-s.a ./build/lib/libtgui-sWin.a
 cp -r ./build/lib/libtgui-sWin.a ./bin/
+
+#Build switch
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_CXX_COMPILER=/opt/devkitpro/devkitA64/bin/aarch64-none-elf-g++ -DTGUI_BACKEND=GLFW_OPENGL3 -DTGUI_SHARED_LIBS=FALSE -DCMAKE_MAKE_PROGRAM=/opt/devkitpro/devkitA64/bin/x86_64-w64-mingw32-make
+#make -j4
+#cd ..
+#mv ./build/lib/libtgui-s.a ./build/lib/libtgui-sSwitch.a
+#cp -r ./build/lib/libtgui-sSwitch.a ./bin/
